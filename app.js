d36c0ab0cd1495f37f45b3fb28f4521adbbff62e
scarly111 <yigitcagri48@gmail.com>
2025-03-08 15:35:33 +0300

    Main functions of the project has been written in pseudocode format.

---
+++ app.js
@@ -0,0 +1,19 @@
+const myLibrary = []
+
+// the book constructor
+function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read
+}
+
+// take a book and add it to the library
+function addBookToLibrary(){
+
+}
+
+function createBook(){
+    
+}
